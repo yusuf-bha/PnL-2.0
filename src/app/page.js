1975f2b874b5483738081f9bc56ce3c933f54996
yusuf <117863392+yusuf-bha@users.noreply.github.com>
2023-06-08 17:37:20 -0400

    created nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,9 @@ export default function Home() {
   return (
     <>
       <form className="">
-        <label className="">
           <input className="" type="text" placeholder="Wallet"></input>
           <input className="" type="text" placeholder="Token"></input>
-          <input className="" type="submit"></input>
-        </label>
+          <input className="" type="submit" value="Search"></input>
       </form>
     </>
   )
---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="w-full h-60px flex items-center relative py-2 bg-white text-black shadow-md">
+        <p className="ml-4 text-2xl no-underline">PnL</p>
+        <div className="ml-auto">
+          <ul className="flex">
+            <li className="list-none mx-4">About</li>
+            <li className="list-none mx-4">Contact</li>
+            <li className="list-none mx-4">History</li>
+            <li className="list-none mx-4">Profile</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
