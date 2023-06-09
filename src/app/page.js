78f24366433805515ae4e7335a6a6c56496f8c01
yusuf <117863392+yusuf-bha@users.noreply.github.com>
2023-06-09 19:20:18 -0400

    working on chart component

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,21 @@
-import fetchData from "@/lib/fetchdata"
-import { useState } from "react"
+import fetchData from "@/lib/fetchdata";
+import { useState } from "react";
+import Chart from "@/components/chart";
 
 export default function Home() {
-  const [displayChart, setDisplayChart] = useState[false];
+  const [displayChart, setDisplayChart] = useState(false);
+  const [chartData, setChartData] = useState({})
+
   const getData = async (event) => {
     const wallet = event.target.wallet.value;
     const token = event.target.token.value;
     data = await fetchData(wallet, token);
     setDisplayChart(true);
+    setChartData({chart info here})
   }
   return (
     <>
-      <form onSubmit={getData()} className="w-full flex justify-center mt-[10%] gap-2.5">
+      <form onSubmit={getData} className="w-full flex justify-center mt-[10%] gap-2.5">
         <input className="border-blue-300 border rounded" type="text" name="wallet" placeholder="Wallet"></input>
         <input className="border-blue-300 border rounded" type="text" name="token" placeholder="Token"></input>
         <input className="bg-blue-300 border-2 rounded" type="submit" value="Search"></input>
@@ -19,4 +23,3 @@ export default function Home() {
     </>
   )
 }
-// render the appropriate info based on display chart
--- src/components/chart.js
+++ src/components/chart.js
@@ -1,7 +1,23 @@
+import { Line } from "react-chartjs-2"
 
-
-export default function Chart() {
+export default function Chart({ chartData }) {
   return (
-    <div>char</div>
+    <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Line Chart</h2>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Users Gained between 2016-2020"
+            },
+            legend: {
+              display: false
+            }
+          }
+        }}
+      />
+  </div>
   )
 }
