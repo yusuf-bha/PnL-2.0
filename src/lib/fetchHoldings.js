9fb2fc9f3c648f007492bae54fdde83d81b74dd2
yusuf <117863392+yusuf-bha@users.noreply.github.com>
2023-06-13 18:12:40 -0400

    wrote the function for fetching current portfolio data

---
+++ src/lib/fetchHoldings.js
@@ -0,0 +1,38 @@
+
+export default async function fetchHoldings(wallet) {
+  const body = JSON.stringify({
+    sql: "SELECT owner.contract_address AS token_address, (owner.balance / 10^tokens.decimals) AS balance, (SELECT price FROM ethereum.token_prices WHERE token_address = owner.contract_address ORDER BY timestamp DESC LIMIT 1) * (owner.balance / 10^tokens.decimals) AS total_usd_value,tokens.name AS name, (SELECT price FROM ethereum.token_prices WHERE token_address = owner.contract_address ORDER BY timestamp DESC LIMIT 1) AS price FROM ethereum.token_owners AS owner JOIN ethereum.tokens AS tokens ON owner.contract_address = tokens.contract_address WHERE owner.owner_address = '{{wallet_address}}' ORDER BY total_usd_value DESC;",
+    parameters: {
+        wallet_address: wallet
+    }
+  });
+
+  let data = await fetch('https://api.transpose.io/sql', {
+    method: 'POST',
+    body: body,
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': 'WbmQyvuapelJYhuZEThTusUGPyGJ8Mez'
+    }
+  })
+  data = await data.json();
+  const output = {
+    total: 0,
+    results: []
+  };
+  let index;
+  let firstTime = true;
+  for (let i = 0; i < data.results.length; i++) {
+    if (data.results[i].price !== null) {
+      output.results.push(data.results[i]);
+      if (firstTime) index = i;
+      firstTime = false;
+      output.total += data.results[i].total_usd_value;
+    }
+  }  
+  const second = data.results.slice(0, index);
+  const sortedResults = output.results.concat(second);
+  output.results = sortedResults;
+
+  return output;
+}
