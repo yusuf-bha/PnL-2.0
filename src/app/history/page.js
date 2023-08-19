0e1de37d83d0175eeadd2d78c303f5ea830144d4
yusuf <117863392+yusuf-bha@users.noreply.github.com>
2023-08-18 21:48:22 -0400

    finished the fetchTxns function to retrieve all the necessary data for the transaction component

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -4,18 +4,24 @@ import { useState, useEffect } from "react";
 import fetchTxns from "@/lib/fetchTxns";
 
 export default function History() {
-
   const [txns, setTxns] = useState([]);
+
   useEffect(() => {
-    setTxns(fetchTxns());
-  }, [])
+    const fetchTransactions = async () => {
+      const output = await fetchTxns('0xbCF7989942C95507c77ef8ade33a185960BEb9bB');
+      setTxns(output);
+    };
+
+    fetchTransactions();
+  }, []);
+
 
   return (
     <>
       <div>history page</div>
-      {txns.map((txn) => {
-        <Transaction key={txn.block} data={txn}></Transaction>
+      {txns.map((txn, index) => {
+        return <Transaction key={index} data={txn}></Transaction>;
       })}
     </>
-  )
+  );
 }
--- src/lib/fetchTxns.js
+++ src/lib/fetchTxns.js
@@ -1,7 +1,7 @@
 
 export default async function fetchTxns(wallet) {
   const body = JSON.stringify({
-    sql: "",
+    sql: "SELECT et.transaction_hash, et.timestamp, et.block_number, et.from_address, et.to_address, (et.value / 1000000000000000000) AS value, ett.contract_address, etkns.name, etkns.symbol, (ett.quantity / POWER(10, etkns.decimals)) AS quantity FROM ethereum.transactions et LEFT JOIN ethereum.token_transfers ett ON et.transaction_hash = ett.transaction_hash LEFT JOIN ethereum.tokens etkns ON ett.contract_address = etkns.contract_address WHERE et.from_address = '{{wallet_address}}' OR et.to_address = '{{wallet_address}}' ORDER BY block_number DESC;",
     parameters: {
         wallet_address: wallet
     }
@@ -16,5 +16,6 @@ export default async function fetchTxns(wallet) {
     }
   })
   data = await data.json();
-  return data;
+  console.log(data);
+  return data.results;
 }
